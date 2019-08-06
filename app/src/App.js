41de4d59ea4ba1c57d2e13bb3dc44cba50b617ce
stevekutz <stkutz@gmail.com>
2019-08-05 20:52:54 -0500

    form input connected to state

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
@@ -10,14 +10,30 @@ class App extends React.Component {
     }
   
   // handlers
-  
+  handleChange = ev => {
+    this.setState ({[ev.target.name]: ev.target.value})
+  }  
+
+
   //render
   
   render() {
     return (
       <div>
         <h4> value is: {this.state.valueInit}</h4>
-      
+        <form>
+          <label> Label text
+            <input 
+              type = 'text'
+              name = 'valueInit'
+              onChange = {this.handleChange}
+            />
+          
+          </label>
+          {/* input OR button here*/}
+          <button> Submit</button>
+        
+        </form>
       </div>
     )
   
